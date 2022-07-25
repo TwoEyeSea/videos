d32422c6880fa3c1867cdc4163272152624f7976
M Alexander Moroney <alexmoroney@gmail.com>
2022-07-24 22:38:48 -0500

    L127 - styling a list

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./VideoItem.css";
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} />
-      //We can also get video Thumbnails from the Youtube API
-      {video.snippet.title}
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      {/* We can also get video Thumbnails from the Youtube API */}
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
   // DON'T FORGET TO RETURN() YOUR JSX SILLY BOY!
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -7,7 +7,7 @@ const VideoList = ({ videos }) => {
     return <VideoItem key={IdleDeadline.videoId} video={video} />;
     // Don't forget to add the mapped video array items as a prop to the VideoItem component
   });
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
