c12917074a98c765ec1dd2765debbb8172ea4712
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-10 22:34:21 -0500

    Videos App (with hooks) L207
    L207 - Refactor App

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,104 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import YouTube from "../APIs/YouTube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 import DisplayToggleButtons from "./DisplayToggle";
-
 import "./DisplayLayout.css";
-class App extends React.Component {
-  state = {
-    videos: [],
-    selectedVideo: null,
-    gridDisplay: "grid-display",
-  };
-  componentDidMount() {
-    this.onTermSubmit("kurzgesagt");
-  }
 
-  onTermSubmit = async (term) => {
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [gridDisplay, setGridDisplay] = useState("grid-display");
+
+  useEffect(() => {
+    onTermSubmit("kurzgesagt");
+  }, []);
+
+  const onTermSubmit = async (term) => {
     const response = await YouTube.get("/search", {
       params: {
         q: term,
       },
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
   };
 
-  videoListBelow = () => {
-    this.setState({ gridDisplay: " " });
+  const videoListBelow = () => {
+    setGridDisplay(" ");
   };
 
-  videoListOnRight = () => {
-    this.setState({ gridDisplay: "grid-display" });
+  const videoListOnRight = () => {
+    setGridDisplay("grid-display");
   };
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <DisplayToggleButtons videoListBelow={this.videoListBelow} videoListOnRight={this.videoListOnRight} />
-        <div className={this.state.gridDisplay}>
-          <VideoDetail video={this.state.selectedVideo} />
-          <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-        </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <DisplayToggleButtons videoListBelow={videoListBelow} videoListOnRight={videoListOnRight} />
+      <div className={gridDisplay}>
+        <VideoDetail video={selectedVideo} />
+        <VideoList onVideoSelect={onVideoSelect} videos={videos} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+// class BApp extends React.Component {
+//   state = {
+//     videos: [],
+//     selectedVideo: null,
+//     gridDisplay: "grid-display",
+//   };
+
+//   componentDidMount() {
+//     this.onTermSubmit("kurzgesagt");
+//   }
+
+//   onTermSubmit = async (term) => {
+//     const response = await YouTube.get("/search", {
+//       params: {
+//         q: term,
+//       },
+//     });
+
+//     this.setState({
+//       videos: response.data.items,
+//       selectedVideo: response.data.items[0],
+//     });
+//   };
+
+//   onVideoSelect = (video) => {
+//     this.setState({ selectedVideo: video });
+//   };
+
+//   videoListBelow = () => {
+//     this.setState({ gridDisplay: " " });
+//   };
+
+//   videoListOnRight = () => {
+//     this.setState({ gridDisplay: "grid-display" });
+//   };
+
+//   render() {
+//     return (
+//       <div className="ui container">
+//         <SearchBar onFormSubmit={this.onTermSubmit} />
+//         <DisplayToggleButtons videoListBelow={this.videoListBelow} videoListOnRight={this.videoListOnRight} />
+//         <div className={this.state.gridDisplay}>
+//           <VideoDetail video={this.state.selectedVideo} />
+//           <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
 
