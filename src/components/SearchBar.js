2e29d79f37615c85647dd28a224469df8928894a
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-15 20:11:45 -0500

    L217 additional deployment Edits for Netlify

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,7 +12,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={submitForm} className="ui form">
         <div className="field">
-          <label>Video Search Field</label>
+          <label>Search for a Video!</label>
           <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
         </div>
       </form>
